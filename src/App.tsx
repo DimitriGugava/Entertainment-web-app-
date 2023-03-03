4fdad606a10d8f56f20d54d18b5e5a238bd2bfd8
Dima Gugava <dimitrigugavinho@gmail.com>
2023-03-03 23:15:58 +0400

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./App.css";
 import Main from "./Main/Main";
 import Signup from "./Sign/Signup";
 import Login from "./Login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
-      {/* <Signup /> */}
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
